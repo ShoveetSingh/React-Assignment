aba1c1d0790a6c7f95523e1110ab19efcf900cf6
@Shoveet2003 <shoveetsingh2002@gmail.com>
2024-01-18 14:31:20 +0530

    Second page Api calling done succesfully

--- my-react-app/src/components/Page.tsx
+++ my-react-app/src/components/Page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+//import Box from '@mui/material/Box';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
+
+interface props {
+  id: number;
+  title: string;
+  body:string;
 }
 
-export default Page
+const Page: React.FC = () => {
+  const [data, setData] = useState<props[]>([]);
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (response.ok) {
+          const json:props[] = await response.json();
+          setData(json);
+        } else {
+          console.error('Error');
+        }
+      } catch (error:any) {
+        console.error(error.message);
+      }
+    };
+
+    
+    fetchDataFromAPI();
+  }, []); 
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'title', headerName: 'Title', width: 200 },
+    { field: 'body', headerName: 'Body', width: 400 },
+  ];
+
+  return (
+    <div style={{ height: 600, width: '100%' }}>
+      <DataGrid rows={data} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+
+};
+
+export default Page;
